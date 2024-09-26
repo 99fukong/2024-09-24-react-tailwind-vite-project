f37674ed466bdcc7c714cf0f996e383a83769632
99fukong <can6364200@163.com>
2024-09-26 21:50:38 +0800

    pictures

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
             <div className="max-w-5xl mx-auto">
                 <Routes>
                     <Route path={"/"} element={<Home/>}/>
-                    <Route path={"/about"} element={<About/>}/>
+                    <Route path={"/pictures"} element={<Pictures/>}/>
                     <Route path={"/contact"} element={<Contact/>}/>
                 </Routes>
             </div>
@@ -19,14 +19,14 @@ function App() {
 
 function Header() {
     return (
-        <div className="p-4 mb-20">
+        <div className="p-4 mb-10">
             <nav className="space-x-4 text-2xl flex justify-center items-center shadow">
                 <Link className="px-8 py-5 block" to="/">Home</Link>
-                <Link className="px-8 py-5 block" to="/about">About</Link>
+                <Link className="px-8 py-5 block" to="/pictures">pictures</Link>
                 <Link className="px-8 py-5 block" to="/contact">Contact</Link>
-                <a href="https://www.youtube.com/channel/UCmcXVCxNwW5rp-KAc8qadbQ"
+                <a href="https://www.fukonglzw.cn/"
                    className="flex gap-2 items-start justify-start">
-                    <span>Youtube</span>
+                    <span>fukong</span>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24"
                          stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round"
@@ -40,31 +40,41 @@ function Header() {
 
 function Home() {
     return (
-        <div className="space-y-16 text-center">
-            <h2 className="text-2xl">This is home.</h2>
-            <div className="flex gap-4 flex-wrap items-start">
-                <img src="http://placekitten.com/200/300" alt=""/>
-                <img src="http://placekitten.com/400/400" alt=""/>
-                <img src="http://placekitten.com/300/300" alt=""/>
-            </div>
+        <div className="flex justify-center flex-col items-center">
+        <img src="https://img.131213.xyz/api/cfile/AgACAgUAAx0Eflp52gACJphm9VfIhK7US4j2PR62ITA1R2C1xAACxr8xG0zXqFdFkPmLZiYWvAEAAwIAA3cAAzYE" alt="" className="rounded-full mb-20" style={{ width: '100px',height: 'auto', objectFit: 'contain' }}/>
+        <h2 className="text-2xl mb-10">Home <a href="https://99fukong-github-io.vercel.app/" target="_blank" className="font-bold uppercase text-blue-400">Hollis</a></h2>        
+            <p>Hello , everyone , I'm Hollis.</p>
         </div>
     )
 }
 
-function About() {
+function Pictures() {
     return (
-        <div className="flex justify-center flex-col items-center">
-            <img src="http://placekitten.com/200/200" alt="" className="rounded-full mb-20"/>
-            <h2 className="text-2xl mb-10">About <a href="https://blog.sombex.com"
-                                                    className="font-bold uppercase text-blue-400">Sombex</a></h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet architecto asperiores culpa cumque
-                dignissimos doloribus ea earum enim est fuga laboriosam, nam nesciunt numquam optio rem soluta sunt
-                velit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque corporis eligendi enim et
-                in iure quaerat qui quisquam quos. Ex facilis id numquam, perferendis praesentium quam quod rem
-                voluptate.
-            </p>
+        <div className="space-y-10 text-center">
+            <h2 className="text-2xl">This is Pictures.</h2>
+            <div className="flex gap-4 flex-wrap items-start">
+                <Image 
+                    src="https://img.131213.xyz/api/cfile/AgACAgUAAx0Eflp52gACJplm9VffFunUY4cRdcp_rsCmeisvmAACx78xG0zXqFcN5OqN2VywZwEAAwIAA3cAAzYE"
+                    width={500} 
+                />
+                <Image 
+                    src="https://img.131213.xyz/api/cfile/AgACAgUAAx0Eflp52gACJppm9VfuUGhDfHRXefMcgerrcln3RwACyL8xG0zXqFfx5qGyjSOwqgEAAwIAA3cAAzYE"
+                    width={500}  
+                />
+            </div>
         </div>
-    )
+    );
+}
+
+function Image({ src, width }) {
+    return (
+        <img 
+            src={src} 
+            alt="" 
+            style={{ width: `${width}px`, height: '500px' }} 
+            // style={{ width: `${width}px`, height: 'auto', objectFit: 'contain' }} 
+        />
+    );
 }
 
 function Contact() {
@@ -72,7 +82,7 @@ function Contact() {
         <div>
             <h2 className="text-2xl mb-10">Contact me</h2>
             <p className="text-lg">Email: <a href="mailto:someone@somewhere.gone"
-                                             className="underline text-blue-700 leading-relaxed">someone@somewhere.gone</a>
+                                             className="underline text-blue-700 leading-relaxed">fukongovo@gmail.com</a>
             </p>
         </div>
     )
